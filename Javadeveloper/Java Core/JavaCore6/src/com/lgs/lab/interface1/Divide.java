d965cbb416964bbac8cb24f6483c53ef37559b21
ariadnaamore <70611011+ariadnaamore@users.noreply.github.com>
2021-08-02 01:39:32 +0300

    Delete Javadeveloper/Java Core/JavaCore6/src directory

--- Javadeveloper/Java Core/JavaCore6/src/com/lgs/lab/interface1/Divide.java
+++
@@ -1,5 +0,0 @@
-package com.lgs.lab.interface1;
-
-public interface Divide {
-public void divide();
-}
--- Javadeveloper/Java Core/JavaCore6/src/hw6Sallary/Salary.java
+++
@@ -1,7 +0,0 @@
-package hw6Sallary;
-
-public interface Salary {
-
-	public void salary();
-
-}
